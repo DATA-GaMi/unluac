d3555f8bbad2106a0984f35cb3b3c03f513d41d5
Desec <48061253+DATA-GaMi@users.noreply.github.com>
2023-08-05 16:45:53 +0800

    upload

--- src/parse/LBooleanType.java
+++ src/parse/LBooleanType.java
@@ -1,2 +1,30 @@
+package unluac.parse;
 
-error: src/unluac/parse/LBooleanType.java @cc131a3e5839: not found in manifest
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+
+public class LBooleanType extends BObjectType<LBoolean> {
+
+  @Override
+  public LBoolean parse(ByteBuffer buffer, BHeader header) {
+    int value = buffer.get();
+    if((value & 0xFFFFFFFE) != 0) {
+      throw new IllegalStateException();
+    } else {
+      LBoolean bool = value == 0 ? LBoolean.LFALSE : LBoolean.LTRUE;
+      if(header.debug) {
+        System.out.println("-- parsed <boolean> " + bool);
+      }
+      return bool;
+    }
+  }
+
+  @Override
+  public void write(OutputStream out, BHeader header, LBoolean object) throws IOException {
+    int value = object.value() ? 1 : 0;
+    out.write(value);
+  }
+  
+}
